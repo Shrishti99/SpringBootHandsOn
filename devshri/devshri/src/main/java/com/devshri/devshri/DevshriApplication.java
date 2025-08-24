1a88acca7b64b440a7c82bd9bfabd27a74e2437b
Shrishti <999shrishti.singh@gmail.com>
2025-08-24 19:22:00 +0530

    added src folder

---
+++ devshri/devshri/src/main/java/com/devshri/devshri/DevshriApplication.java
@@ -0,0 +1,13 @@
+package com.devshri.devshri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevshriApplication {
+//    spring boot application
+	public static void main(String[] args) {
+		SpringApplication.run(DevshriApplication.class, args);
+	}
+
+}
---
+++ devshri/devshri/src/test/java/com/devshri/devshri/DevshriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshri.devshri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
