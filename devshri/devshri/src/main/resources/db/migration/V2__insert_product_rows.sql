d70277496ecc0c23f4baa70ff3fba989e6658ce9
Dgit-10 <dbajmera2@gmail.com>
2025-08-26 21:36:02 +0530

    Added V2 and V3 files for migration in DB

---
+++ devshri/devshri/src/main/resources/db/migration/V2__insert_product_rows.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCT (type, name, price, quantity, description) VALUES
+  ('Electronics', 'Smartphone', 699.99, 50, 'Latest model smartphone'),
+  ('Electronics', 'Laptop', 1299.99, 30, 'High performance laptop'),
+  ('Home', 'Vacuum Cleaner', 199.99, 100, 'Powerful vacuum cleaner');
